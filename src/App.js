4e5d41a9f89aaab348f67aa92926b530a6d44117
WooYoung94 <ddsksw01@gmail.com>
2019-12-18 15:42:50 +0900

    3.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function App() {
-  return <div className="App" />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("const");
+  };
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  componentDidMount(){
+    console.log("componentDidMount")
+  };
+  componentDidUpdate(){
+    console.log("componentDidUpdate")
+  };
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+    }
   }
 
 export default App;
